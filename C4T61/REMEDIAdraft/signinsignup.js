11ce044ab9c495a13744e759d127cd6f86885fb5
nerissavu <vuthunga2002@gmail.com>
2020-07-20 16:53:27 +0700

    new commit

---
+++ C4T61/REMEDIAdraft/signinsignup.js
@@ -0,0 +1,48 @@
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBBDyuPsfj12PsNsQYtOm8wTOIEGfEqzVs",
+  authDomain: "remedia-cc73b.firebaseapp.com",
+  databaseURL: "https://remedia-cc73b.firebaseio.com",
+  projectId: "remedia-cc73b",
+  storageBucket: "remedia-cc73b.appspot.com",
+  messagingSenderId: "884410707515",
+  appId: "1:884410707515:web:c656e2c124dc160edf761e",
+  measurementId: "G-6SS4LXEEKS"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+function realsignUp(){
+
+  var name = document.getElementById("suname")
+  var email= document.getElementById("suemail")
+  var password = document.getElementById("supassword")
+
+  const promise = auth.createUserWithEmailAndPassword (name.value, email.value, password.value)
+  promise.catch(e => alert(e.message));
+
+  alert("Signed Up");
+}
+
+function realsignIn(){
+  var email= document.getElementById("siemail")
+  var password = document.getElementById("sipassword")
+
+  const promise = auth.SignInWithEmailAndPassword (email.value, password.value)
+  promise.catch(e => alert(e.message));
+
+  alert("Signed In" );
+}
