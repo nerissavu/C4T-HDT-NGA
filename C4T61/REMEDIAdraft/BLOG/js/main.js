e77b93d0ef6ce9604613b8fafc2194f637d957fe
nerissavu <vuthunga2002@gmail.com>
2020-08-02 10:24:42 +0700

    new commit

--- C4T61/REMEDIAdraft/BLOG/js/main.js
+++ C4T61/REMEDIAdraft/BLOG/js/main.js
@@ -1,3 +1,18 @@
+const responsive = {
+    0: {
+        items: 1
+    },
+    400: {
+        items: 1
+    },
+    640: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
 $(document).ready(function () {
 
     $nav = $('.nav');
@@ -8,22 +23,34 @@ $(document).ready(function () {
         $nav.toggleClass('collapse');
     })
 
-});
+
 
     // owl-crousel for blog
-    $('.owl-carousel').owlCarousel();
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
+    });
 
 
     // click to scroll down
     $('.move-down button').click(function () {
         $('html, body').animate({
-            scrollTop: 1000
-        }, 3000);
+            scrollTop: 5200
+        }, 15000);
     })
     
     // click to scroll top
     $('.move-up span').click(function () {
         $('html, body').animate({
             scrollTop: 0
-        }, 1000);
+        }, 3000);
     })
+
+    // AOS Instance
+    AOS.init();
+});
